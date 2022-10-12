e41af49361cd31c762c200521d9ccd06c1947318
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-10-12 16:14:42 -0300

    prepare for deploy

--- src/db/db.js
+++ src/db/db.js
@@ -3,11 +3,10 @@ import pg from "pg";
 const { Pool } = pg;
 
 const databaseConfig = {
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '123456',
-  database: 'shortly'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
 
 const connection = new Pool(databaseConfig);
